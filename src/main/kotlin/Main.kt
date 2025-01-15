ffca32374880bb7d3ce024e96996370b76b75c36
BazhenovVS <bazhenov4job@gmail.com>
2025-01-15 19:41:08 +0300

    Сделать Задачу №1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,11 @@ package ru.netology
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val amount = 10000
+    val tax = 0.0075
+    val min_tax = 35
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val result = if ((amount * tax) < 35) min_tax else amount * tax
+
+    println("Размер комиссии составляет $result рублей.")
 }
---
+++ src/main/kotlin/Main_2.kt
@@ -0,0 +1,4 @@
+package ru.netology
+
+class Main_2 {
+}
