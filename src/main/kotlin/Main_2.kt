2ba07dae2ea9a411a4d4306cb00f491106307fd7
BazhenovVS <bazhenov4job@gmail.com>
2025-01-15 21:47:22 +0300

    Задача №2 Учесть кейс, когда число заканчивается на 11

--- src/main/kotlin/Main_2.kt
+++ src/main/kotlin/Main_2.kt
@@ -1,11 +1,11 @@
 package ru.netology
 
 fun main() {
-    val likes = 41
+    val likes = 111
     val verbose = "людям"
     val uni = "человеку"
 
-    val result = if (likes % 10 == 1) uni else verbose
+    val result = if ((likes % 10 == 1) && !(likes.toString().endsWith("11"))) uni else verbose
 
     println("Понравилось $likes $result")
 }
